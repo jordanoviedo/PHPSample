e7b80ba50ee17ec921807b42639a661c7a5a3951
jordanoviedo <jordansoy@gmail.com>
2016-05-07 19:00:49 +0200

    index.php
    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $variableUno = 1;
+            $variableDos = $variableUno + 1;
+            echo $variableDos;
+            
+            $variableTres= "Hola ";
+            $variableCuatro = $variableTres . "Hola ";
+            echo $variableCuatro;
+        ?>
+    </body>
+</html>
